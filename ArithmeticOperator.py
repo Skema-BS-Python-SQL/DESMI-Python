56b002c3977c69a2d792b01953ca77045a8f84a4
Mike Castro Demaria <mike.castrodemaria-ext@skema.edu>
2025-05-13 15:22:19 +0200

    Create ArithmeticOperator.py

---
+++ ArithmeticOperator.py
@@ -0,0 +1,22 @@
+# Arithmetic operators are used with numeric values 
+# to perform common mathematical operations:
+
+x = 5
+y = 3
+
+# Addition
+print(x + y)
+# Subtraction
+print(x - y)
+# Multiplication
+print(x * y)
+# Division
+print(x / y)
+# Modulus
+print(x % y)
+# Exponentiation
+#same as 2*2*2*2*2
+print(x ** y) 
+# Floor division	
+#the floor division // rounds the result down to the nearest whole number
+print(x // y)
