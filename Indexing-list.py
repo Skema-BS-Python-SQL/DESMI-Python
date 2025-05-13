72673e2090f533878ef316daae8dd0e903034e3d
Mike Castro Demaria <mike.castrodemaria-ext@skema.edu>
2025-05-13 15:29:58 +0200

    Create Indexing-list.py

---
+++ Indexing-list.py
@@ -0,0 +1,43 @@
+# A Python list uses the zero-based numbering system.
+# Meaning, all its items are uniquely identified by an index number
+# starting from 0 to n-1 where n is the length of the list.
+#
+colors = ['red', 'blue', 'green', 'yellow', 'black', 'purple','turquoise']  # create list
+print(colors)
+
+print(colors[0])
+print(colors[0:2])
+
+#colors.pop(2)
+#colors.remove('green')
+#colors.append('maroon')
+
+#print(colors)
+
+#colors.pop(0)
+#colors.remove('green')
+
+#print(colors)
+
+#colors.sort()
+#print(colors)
+
+#colors.reverse()
+#print(colors)
+
+#num = len(colors)
+#print(num)  # get list length 5
+
+#print(colors[0])
+#first = colors[0]
+#last = colors[-1]
+#print(first, last)
+#print(colors[0::4])
+
+#colors = ['red', 'blue', 'green', 'yellow', 'black', 'purple','turquoise']  # create list
+
+#print("--------\n")
+
+#for eachcolor in colors:
+#    if eachcolor != 'green':
+#        print(eachcolor)
