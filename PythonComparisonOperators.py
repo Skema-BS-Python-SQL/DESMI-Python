bb4be8c9ea0c21eb8302fa32a13b912545364d8c
Mike Castro Demaria <mike.castrodemaria-ext@skema.edu>
2025-05-13 15:20:25 +0200

    Create PythonComparisonOperators.py

---
+++ PythonComparisonOperators.py
@@ -0,0 +1,26 @@
+# Operator, Name, Example
+x = 5
+y = 3
+# ==	Equal	x == y
+# returns False because 5 is not equal to 3
+print(x == y)
+
+# returns True because 5 is not equal to 3
+#!=	Not equal	x != y
+print(x != y)
+
+#>	Greater than	x > y	
+# returns True because 5 is greater than 3
+print(x > y)
+
+#<	Less than	x < y	
+# returns False because 5 is not less than 3
+print(x < y)
+
+#>=	Greater than or equal to	x >= y
+# returns True because five is greater, or equal, to 3
+print(x >= y)
+
+#<=	Less than or equal to	x <= y
+# returns False because 5 is neither less than or equal to 3
+print(x <= y)
