46c82cccbc587e80dd4163ef12b04895f9a08bb8
Mike Castro Demaria <mike.castrodemaria-ext@skema.edu>
2025-05-13 14:48:42 +0200

    Create Python_String_to_list.py

---
+++ Python_String_to_list.py
@@ -0,0 +1,9 @@
+# Convert list to string
+# list is "Mike, Paul, Pierre, Stéphane, Max, Jean"
+#
+# split fuction: 
+# https://www.w3schools.com/python/ref_string_split.asp
+#
+tstring="Mike, Paul, Pierre, Stéphane, Max, Jean"
+newlist=tstring.split(", ")
+print(newlist)
