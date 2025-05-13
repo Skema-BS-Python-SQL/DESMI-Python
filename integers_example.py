3ef894fb89b0a14f87502df937e3083f6b89b7db
Mike Castro Demaria <mike.castrodemaria-ext@skema.edu>
2025-05-13 15:32:51 +0200

    Create integers_example.py

---
+++ integers_example.py
@@ -0,0 +1,13 @@
+n1 = 20 + 10
+n2 = 20 - 10
+n3 = 20 * 10
+n4 = 20 / 10
+print(str(n1) + "\n" + str(n2) + "\n" + str(n3) + "\n" + str(n4))
+# To calculate exponents, you use two multiplication symbols (**). 
+n5 = 3**3
+print(n5)
+# To modify the order of operations, you use the parentheses ()
+n6 = 20 / (10 + 10)
+print(n6)
+n7 = 10 + 10 / 20
+print(n7)
